995362a978a41a57aa3833280543f2419c358fc6
Dominik Heiser <dominik-heiser@web.de>
2023-12-19 15:53:57 +0100

    first commit

---
+++ python.py
@@ -0,0 +1,23 @@
+		def task1(a, b):
+		return not a and not b
+		
+		def task2(a, c):
+		return a and c or a and not c
+		
+		def task3():
+		return False
+		
+		def task4(a, b, c):
+		return a or not b and not c
+		
+		def task5(a ,b):
+		return a and not b
+		
+		def task6(a, c):
+		return c or d
+		
+		def task7(a, c):
+		return not a or not c
+		
+		def task8(a, c):
+		return not a or not c
